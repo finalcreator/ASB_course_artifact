b99da8452b3f3eda7ce470371d3ca0cd329d6fbe
igndukwe <igndukwe>
2022-10-26 06:26:58 +0000

    update

--- formular1/ingestion/0.ingest_all_files.py
+++ formular1/ingestion/0.ingest_all_files.py
@@ -3,11 +3,13 @@ dbutils.notebook.help()
 
 # COMMAND ----------
 
-# ltes call 8b.ingestion_parameterize_nbs from this noteook and add a parameter value to it before the run
-dbutils.notebook.run("8b.ingestion_parameterize_nbs", 0, {"param_data_source": "Ergast API"})
+# lets call 8b.ingestion_parameterize_nbs from this noteook and add a parameter value to it before the run
+value_result = dbutils.notebook.run("8b.ingestion_parameterize_nbs", 0, {"param_data_source": "Ergast API"})
 
 # We can now click on the notebook job to open it
 
 # COMMAND ----------
 
-
+## Hence if we want to chain other nothebooks to run 
+# we can write a conditional stmt to run only if this is a success
+value_result
