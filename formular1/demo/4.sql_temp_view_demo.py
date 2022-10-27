dc785fe5d2c5bac3e6ad4b8d4fda93942bad1b80
igndukwe <igndukwe>
2022-10-27 08:43:07 +0000

    update

--- formular1/demo/4.sql_temp_view_demo.py
+++ formular1/demo/4.sql_temp_view_demo.py
@@ -30,21 +30,21 @@ race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
 
 # COMMAND ----------
 
-#race_results_df.createTempView("vw_race_results")
-race_results_df.createOrReplaceTempView("vw_race_results")
+#race_results_df.createTempView("tvw_race_results")
+race_results_df.createOrReplaceTempView("tvw_race_results")
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC SELECT *
-# MAGIC FROM vw_race_results
+# MAGIC FROM tvw_race_results
 # MAGIC WHERE race_year = 2020
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC SELECT COUNT(*)
-# MAGIC FROM vw_race_results
+# MAGIC FROM tvw_race_results
 # MAGIC WHERE race_year = 2020
 
 # COMMAND ----------
@@ -52,7 +52,7 @@ race_results_df.createOrReplaceTempView("vw_race_results")
 # Running SQL stmts from python
 # gives you the ability to put sql data into a dataframe
 # hence you can now use the datafframe to do whatever
-race_results_2019_df = spark.sql("SELECT * FROM vw_race_results WHERE race_year = 2019")
+race_results_2019_df = spark.sql("SELECT * FROM tvw_race_results WHERE race_year = 2019")
 
 # COMMAND ----------
 
@@ -62,11 +62,57 @@ display(race_results_2019_df)
 
 # another benefit of running SQL smts from python is that you can parameterize
 param_race_year = 2019
-race_results_2019_df = spark.sql(f"SELECT * FROM vw_race_results WHERE race_year = {param_race_year}")
+race_results_2019_df = spark.sql(f"SELECT * FROM tvw_race_results WHERE race_year = {param_race_year}")
 
 # COMMAND ----------
 
 display(race_results_2019_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Global Temp Views
+# MAGIC 1. Create global views on dataframes
+# MAGIC 2. Access the view from SQL cell
+# MAGIC 3. Access the view from python cell
+# MAGIC 4. Access the view from another notebook
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC * A global temp view is avaliable accross the entire application
+
+# COMMAND ----------
+
+#race_results_df.createGlobalTempView("vw_race_results")
+race_results_df.createOrReplaceGlobalTempView("gvw_race_results")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES IN global_temp;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT *
+# MAGIC FROM global_temp.gvw_race_results
+
+# COMMAND ----------
+
+# Running SQL stmts from python
+# gives you the ability to put sql data into a dataframe
+# hence you can now use the datafframe to do whatever
+race_results_2019_df = spark.sql("SELECT * FROM global_temp.gvw_race_results WHERE race_year = 2019")
+
+# COMMAND ----------
+
+race_results_2019_df.show()
 
 # COMMAND ----------
 
--- formular1/demo/5.sql_temp_view_demo.py
+++ formular1/demo/5.sql_temp_view_demo.py
@@ -1,7 +1,15 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC SELECT *
-# MAGIC FROM vw_race_results
+# can not access temp view from another notebook
+%sql
+SELECT *
+FROM tvw_race_results
+
+# COMMAND ----------
+
+# can access a global temp view from another notebook
+%sql
+SELECT *
+FROM global_temp.gvw_race_results
 
 # COMMAND ----------
 
